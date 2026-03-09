4753818cce8837a3b601337c1a40fdc46348dfb4
Alex Pereira <pereraacompany@gmail.com>
2026-03-08 23:29:16 -0300

    feat: conversor de (Fahrenheit para Celsius)
    Este programa em C# realiza a conversão de temperaturas de Fahrenheit para graus Celsius.
    
    O que foi aplicado no código:
    - Implementação da fórmula matemática de conversão de forma otimizada.
    - Uso de `double.Parse` para leitura de dados do usuário.
    - Formatação de saída de dados utilizando interpolação de strings ($"") com limitação de duas casas decimais (:N2).
    - Boas práticas de nomenclatura de variáveis (Clean Code) e usabilidade no terminal.

---
+++ Conversor_De_Temperadura_Cel&Fah/Conversor_De_Temperadura_Cel&Fah/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor_De_Temperadura_Cel_Fah
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a temperatura em Fahrenheith:");
+            //double fah = double.Parse(Console.ReadLine());
+            double fahrenheith = double.Parse(Console.ReadLine());
+
+            //double cel = (fah - 32) / 1.8;
+            //Console.WriteLine("A temperatura em Celsius é: " + cel.ToString("N"));
+            double celsius = (fahrenheith - 32) / 1.8;
+            Console.WriteLine($"A temperatura em Celsius é: {celsius:N2}°C");
+        }
+    }
+}
---
+++ Conversor_De_Temperadura_Cel&Fah/Conversor_De_Temperadura_Cel&Fah/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Conversor_De_Temperadura_Cel&Fah")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Conversor_De_Temperadura_Cel&Fah")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("b2aabfbf-1c38-4d03-8852-e4a62816537d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
