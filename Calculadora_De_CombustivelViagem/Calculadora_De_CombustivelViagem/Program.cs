fb69b6137988206a1b2b329063ce627e9c3eac63
Alex Pereira <pereraacompany@gmail.com>
2026-03-08 23:31:05 -0300

    feat: Cal custo de viagem e consumo de combustível
    Este programa em C# calcula o custo total de uma viagem baseando-se na distância percorrida, no consumo médio do veículo e no valor atual do combustível.
    
    O que foi aplicado no código:
    - Leitura de múltiplos dados de entrada numéricos utilizando `double.Parse`.
    - Lógica matemática para o cálculo de litros gastos e valor total da viagem.
    - Melhoria na UX (User Experience) do terminal, adicionando unidades de medida claras nas perguntas (km, km/l).
    - Formatação automática do resultado final no formato de moeda local utilizando a interpolação de strings (:C).

---
+++ Calculadora_De_CombustivelViagem/Calculadora_De_CombustivelViagem/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_De_CombustivelViagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Bem Vindo a Calculadora de Combustivel para Viagem! ---");
+
+            //Console.WriteLine("Digite a distâcia percorrida:");
+            Console.Write("Digite a distância percorrida (em km): ");
+            double distancia = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o valor do combustivel:");
+            //double valorC = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor médio do combustivel (km/l): ");
+            double consumo = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o consumo:");
+            //double consumo = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do combustível (por litro): ");
+            double valorCombustivel = double.Parse(Console.ReadLine());
+
+            //double total = ((distancia / consumo) * valorC);
+            //Console.WriteLine("O Total da viagem é :" + total);
+
+            double totalGasto = ((distancia / consumo) * valorCombustivel);
+            Console.WriteLine($"\nO custo total da viagem será de: {totalGasto:C}");
+        }
+    }
+}
